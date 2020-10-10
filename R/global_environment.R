55dee8180c5e0799904e69a6484cfb0386dd74bb
xuzhenwu <xuzhw5@mail2.sysu.edu.cn>
2020-10-10 20:59:29 +0800

    try to test slibrary

--- R/global_environment.R
+++ R/global_environment.R
@@ -1,11 +1,31 @@
-# There are some system settings for personal use
-# With lots of fuctions
-
-# 1-slibrary, install the package before library when the package was not installed
-slibrary <- function(fun){
-  fun_name <- as.character(substitute(fun))
-  if(is.element(fun_name, installed.packages()[,1]) == FALSE)
-    install.packages(fun_name)
-  COMMAND <- paste("library(", fun_name, ")", sep = "")
-  eval(parse(text = COMMAND))
+#' safe mode for library
+#' check if it is installed before library
+#'
+#' @param fun the name of the packages
+#' @param install automatically install the uninstalled packages
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' slibrary(lattice)
+#' slibrary(lattice, install = FALSE)
+#'
+slibrary <- function(..., install = TRUE){
+  funs <- as.character(substitute(list(...)))
+  for(i in seq_along(funs)){
+    fun_name <- funs[i]
+    if(fun_name != "install" & fun_name != "list"){
+      if(is.element(fun_name, installed.packages()[,1]) == FALSE){
+        if(install == TRUE){
+          install.packages(fun_name)
+          COMMAND <- paste("library(", fun_name, ")", sep = "")
+          eval(parse(text = COMMAND))
+        }
+        else{
+          print(paste("package '", fun_name, "' is not installed", sep = ""))
+        }
+      }
+    }
+  }
 }
